a8bd9cbea48cb87595146cc6e4f69a0ee93f85a3
yonta <913325816@qq.com>
2024-03-24 15:00:09 +0800

    fix(): add faster solution

--- src/leetcode15.java
+++ src/leetcode15.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.AbstractList;
 
 public class leetcode15 {
     /**
@@ -34,5 +35,65 @@ public class leetcode15 {
         }
         result.addAll(s);
         return result;
+    }
+
+
+    private List<List<Integer>> res;
+    /**
+     * faster solution
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum2List(int[] nums) {
+        return new AbstractList<List<Integer>> () {
+            @Override
+            public int size() {
+                init();
+                return res.size();
+            }
+
+            @Override
+            public List<Integer> get(int index) {
+                init();
+                return res.get(index);
+            }
+
+            private void init() {
+                if (res != null) {
+                    return;
+                }
+                Arrays.sort(nums);
+
+                long partialSum;
+                Set < List < Integer >> output = new HashSet<> ();
+                for (int i = 0; i < nums.length - 2; i++) {
+                    if (i > 0 && nums[i] == nums[i - 1]) {
+                        continue;
+                    }
+                    partialSum = nums[i];
+                    int begin = i + 1;
+                    int end = nums.length - 1;
+                    while (begin < end) {
+                        long partial = partialSum + nums[begin] + nums[end];
+                        if (partial == 0) {
+                            //add to solution
+                            output.add(Arrays.asList(nums[i], nums[begin], nums[end]));
+                        }
+                        if (partialSum + nums[begin] + nums[end] > 0) {
+                            end--;
+                            while (nums[end] == nums[end + 1] && begin < end) {
+                                end--;
+                            }
+                        } else {
+                            begin++;
+                            while (nums[begin] == nums[begin - 1] && begin < end) {
+                                begin++;
+                            }
+                        }
+                    }
+                }
+                res = new ArrayList<>(output);
+            }
+        };
     }
 }
