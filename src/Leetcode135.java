8748da865854df8f7ac763cd47296185f082cf44
yonta <913325816@qq.com>
2024-08-31 15:11:22 +0800

    feat(): add leetcode 135th problem solution

---
+++ src/Leetcode135.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Leetcode135 {
+    /**
+     * Candy
+     * @param ratings
+     * @return
+     */
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        return Arrays.stream(candies).sum();
+    }
+}
