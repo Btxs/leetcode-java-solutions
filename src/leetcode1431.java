ad53f0b8e2cda424d4f9eb5d0e08571ce00b413e
yonta <913325816@qq.com>
2024-08-31 21:26:17 +0800

    feat(): add leetcode 1431th problem solution

---
+++ src/leetcode1431.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode1431 {
+    // Kids With the Greatest Number of Candies
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            maxCandies = Math.max(maxCandies, candy);
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            if (candy + extraCandies >= maxCandies) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
