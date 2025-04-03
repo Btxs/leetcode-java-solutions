e354e2d57dc72202e526746435e20a281b2bb098
yonta <yonta2023@outlook.com>
2025-04-03 21:10:52 +0800

    feat: Add solution for Leetcode 2874 Maximum Value of an Ordered Triplet II

---
+++ src/Leetcode2874.java
@@ -0,0 +1,14 @@
+public class Leetcode2874 {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        long res = 0;
+        long imax = 0;
+        long dmax = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dmax * nums[i]);
+            dmax = Math.max(dmax, imax - nums[i]);
+            imax = Math.max(imax, nums[i]);
+        }
+        return res;
+    }
+}
